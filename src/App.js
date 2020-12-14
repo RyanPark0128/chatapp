7111999126c4d3d19fb89e681372c0425eee55d5
RyanPark0128 <vazywon@hotmail.com>
2020-12-14 13:21:54 -0800

    styling: add new main page

--- src/App.js
+++ src/App.js
@@ -52,16 +52,16 @@ function SignIn() {
 
       <div className="contents">
         <div className="title">
-          SimplyChat
+          Chat, and have fun
         </div>
         <div className="subtitle">
           Simply sign in and start chatting
         </div>
         <div className="google">
           <div>
-            <a class="btn btn-outline-dark" onClick={signInWithGoogle} role="button" style={{ textTransform: 'none' }}>
-              <img width="20px" style={{ marginBottom: '3px', marginRight: '5px' }} alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-              Login with Google
+            <a class="btn btn-outline-dark" onClick={signInWithGoogle} role="button" style={{ textTransform: 'none', backgroundColor: "white", width: "24.1%" }}>
+              <img width="20px" style={{ backgroundColor: "white" , marginBottom: '3px', marginRight: '5px' }} alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+              Sign in with Google
             </a>
           </div>
         </div>
